eeb3cd93020a490d2e3371c309267cae0753678b
Facundo Viale <91499618+GreyshadowARG@users.noreply.github.com>
2023-04-21 13:58:22 -0300

    ultima version reservas

--- src/controllers/reservation.controller.js
+++ src/controllers/reservation.controller.js
@@ -4,20 +4,35 @@ export const newReservation = async (req, res) => {
   const { id, fecha, hora, personas, descuento } = req.body;
   const restaurant = await Restaurant.findById(id);
   const dbReservaArray = restaurant.reservas;
-  const reservationId = Math.floor(Math.random() * 1000000000);
-  const reserva = { 
+  let fechaCount = 0;
+  let fechasArray = [];
+  dbReservaArray.forEach(function (arrayItem) {
+    var fecha = arrayItem.fecha;
+    fechasArray.push(fecha);
+  });
+  fechasArray.forEach(function (element) {
+    if (element === fecha) {
+      fechaCount++;
+    }
+  });
+  if (fechaCount < 10) {
+    const reservationId = Math.floor(Math.random() * 1000000000);
+    const reserva = {
       id: reservationId,
       fecha: fecha,
       hora: hora,
       personas: personas,
-      descuentos: descuento
-  };
-  dbReservaArray.push(reserva);
-  const newReserva = {
-    reservas: dbReservaArray,
-  };
-  await Restaurant.findByIdAndUpdate(restaurant, newReserva);
-  res.json("Reserva cargada");
+      descuentos: descuento,
+    };
+    dbReservaArray.push(reserva);
+    const newReserva = {
+      reservas: dbReservaArray,
+    };
+    await Restaurant.findByIdAndUpdate(restaurant, newReserva);
+    res.json(dbReservaArray);
+  } else {
+    res.json("No hay reservas disponibles")
+  }  
 };
 
 export const checkAvailability = async (req, res) => {
